ae2b75953609a56f16a396b2767ab54e77848432
Ahmad Mihdawy <ahmedmihdawy@gmail.com>
2025-02-20 19:28:29 +0300

    adding widgit behaver

---
+++ src/app/components/animal-statistic/animal-statistic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-statistic',
+  imports: [],
+  templateUrl: './animal-statistic.component.html',
+  styleUrl: './animal-statistic.component.scss'
+})
+export class AnimalStatisticComponent {
+
+}
