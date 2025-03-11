f8d3112981d138869d17c6cd106143107e661129
Ahmad Mihdawy <ahmedmihdawy@gmail.com>
2025-03-11 07:05:06 +0300

    fix scss

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -70,89 +70,10 @@ export class DashboardService {
     });
   });
 
+  addWidget(w: Widget) {
+    //create anther version of the added widgets and not even the same widget that has been added
+    this.addedWidgets.set([...this.addedWidgets(), { ...w }]);
+  }
+
   constructor() {}
 }
-
-// import { Injectable, signal } from '@angular/core';
-// import { Widget } from '../../interfaces/widget';
-// import { UsersComponent } from '../compoent/users/users.component';
-// import { ChartComponent } from '../compoent/chart/chart.component';
-// import { InformationComponent } from '../compoent/information/information.component';
-
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class DashboardService {
-//   dragEnabled = signal(true); // Control drag & drop activation
-
-//   EnableEdit = signal(true);
-//   widgets = signal<Widget[]>([
-
-//             // {text: 'One', cols: 9, rows: 1, color: 'lightblue'},
-
-//     {
-//       id: 0,
-//       name: 'Users Statistic',
-//       content: UsersComponent,
-//       order: 1,
-//       height: 1 ,
-//       width: 3,
-//       selected :false,
-//       color: 'lightblue'
-//     },
-
-//   // {text: 'Two', cols: 3, rows: 3, color: 'lightgreen'},
-
-//     {
-//       id: 1,
-//       name: 'Animal Statistic',
-//       content: ChartComponent,
-//       order: 2,
-//       height: 2,
-//       width: 1,
-//       selected :false,
-//       color: 'lightgreen'
-//     },
-
-//       // {text: 'Three', cols: 3, rows: 1, color: 'lightpink'},
-
-//     {
-//       id: 2,
-//       name: 'information',
-//       content: InformationComponent,
-//       order: 3,
-//       height: 1,
-//       width: 1,
-//       selected :false,
-//        color: 'lightpink'
-
-//     },
-
-//   // {text: 'Four', cols: 6, rows: 1, color: '#DDBDF1'},
-
-//     {
-//       id: 3,
-//       name: 'information',
-//       content: InformationComponent,
-//       order: 4,
-//       height: 1,
-//       width: 2,
-//       selected :false,
-//        color:'#DDBDF1'
-
-//     },
-//   ]);
-
-//   unUsedWidgets = signal<Widget[]>([])
-
-//   updateWidgets(newOrder: any[]) {
-//     this.widgets.set(newOrder); // Updates the signal state
-//   }
-
-//   removeWidgetByIndex(index: number) {
-//     this.widgets.update(widgets => widgets.filter((_, i) => i !== index));
-//   }
-
-//   constructor() {}
-
-// }
